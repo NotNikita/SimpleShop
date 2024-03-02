9ecd36d1384ecee4452a1ef4749b809beefd44ef
Nik Y. <nikitabsuir@gmail.com>
2024-03-02 19:46:48 +0100

    feat: adding license

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,12 @@ import cors from "cors";
 import express, { Express, Response } from "express";
 import helmet from "helmet";
 
+const port = process.env.PORT || 7777;
+
 const app: Express = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-const port = process.env.PORT || 7777;
 
 app.get("/", (_, res: Response) => {
   res.status(200).send("All good to go!");
